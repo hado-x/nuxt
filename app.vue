56e2a9e6c31965a77d2549aa7c8dcb98941c5e4c
hado-x <hadoyeni@gmail.com>
2025-01-20 12:43:05 +0300

    create a project

---
+++ app.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+const isFetching = ref<boolean>(true)
+</script>
+
+<template>
+  <div class="w-screen h-12 border bg-red-400">
+    <UButton :loading="isFetching" :disabled="isFetching"  variant="soft" icon="heroicons:user" />
+  </div>
+</template>
